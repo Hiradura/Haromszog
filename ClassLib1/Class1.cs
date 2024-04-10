64d80e23cc195450f2bd504ab99aa8f0cfa6c2a1
Orosz Oliver <oroszoliverarnold@ktch.hu>
2024-04-10 11:49:40 +0200

    Add project files.

---
+++ ClassLib1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Channels;
+
+namespace ClassLib1
+{
+    public class Class1
+    {
+        public double SideA;
+        public double SideB;
+        public double SideC;
+
+        public Class1(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        public double CalcPerimeter()
+        {
+            return SideA + SideB + SideC / 2;
+        }
+
+        public double CalcArea()
+        {
+            double s = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+        }
+
+        public override string? ToString()
+        {
+            return $"{CalcArea}, {CalcPerimeter}";
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿using ClassLib1;
+
+Class1 t = new Class1(10, 15, 20);
+
+Console.WriteLine(t);
